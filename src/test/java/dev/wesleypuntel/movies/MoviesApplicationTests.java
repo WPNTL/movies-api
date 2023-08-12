ca0d543a78fbed224ce643e724bfe9e31c1ae5f6
Wesley Puntel Carvalho <wesleypuntel@gmail.com>
2023-08-12 15:28:18 -0300

    my first commit

---
+++ src/test/java/dev/wesleypuntel/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wesleypuntel.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
