efc2a66f84a55808ca3315c0eacec23bf1bd8226
Wesley Puntel Carvalho <wesleypuntel@gmail.com>
2023-08-12 23:49:03 -0300

    Review Class initial config

--- src/main/java/dev/wesleypuntel/movies/Movie.java
+++ src/main/java/dev/wesleypuntel/movies/Movie.java
@@ -2,15 +2,18 @@ package dev.wesleypuntel.movies;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     @Id
     private ObjectId id;
@@ -21,5 +24,7 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
 
 }
---
+++ src/main/java/dev/wesleypuntel/movies/MovieController.java
@@ -0,0 +1,4 @@
+package dev.wesleypuntel.movies;
+
+public class MovieController {
+}
